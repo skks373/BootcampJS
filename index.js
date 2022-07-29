33e5bcd5ecb71e6f8115227bb4d9d909a6f0086f
Cristi Spiru <cristispiru@yahoo.com>
2022-07-29 12:59:54 +0300

    Added validation middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
+import bodyParser from "body-parser";
 import errorsMiddleware from "./src/middleware/errorsMiddleware.js";
 import usersRouter from "./src/routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
-import bodyParser from "body-parser";
 import usersController from "../controllers/userController.js";
 import requestMiddleware from "../middleware/requestMiddleware.js";
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+import validationMiddleware from "../middleware/validationMiddleware.js";
+import { check } from "express-validator";
 
 const router = express.Router();
 
@@ -12,9 +11,15 @@ router.get("/:id(\\d+)", requestMiddleware, usersController.getUser);
 router.get("/test", usersController.fakeGetUser2);
 router.get("/:id(\\w+)", usersController.fakeGetUser);
 
-router.post("/", urlencodedParser, usersController.addUser);
-router.patch("/:id(\\d+)", urlencodedParser, usersController.updateUser);
-router.put("/:id(\\d+)", urlencodedParser, usersController.updateUser2);
+router.post("/", [
+    check("name", "Invalid name, it must have at least 4 characters").isLength({ min: 4 }),
+    check("age", "Invalid age, must be over 18").isInt({ min: 18 }),
+    check("location").exists()
+], validationMiddleware, usersController.addUser);
+
+router.patch("/:id(\\d+)", usersController.updateUser);
+
+router.put("/:id(\\d+)", usersController.updateUser2);
 
 router.delete("/:id(\\d+)", usersController.deleteUser);
 
