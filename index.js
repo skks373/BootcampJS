d97175648b18944b08f516c7cbc783f22d72e2f7
Cristi Spiru <cristispiru@yahoo.com>
2022-08-01 23:42:14 +0300

    Added routing examples

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ import bodyParser from "body-parser";
 import errorsMiddleware from "./src/middleware/errorsMiddleware.js";
 import usersRouter from "./src/routes/userRoutes.js";
 
+const router = express.Router();
+
+router.route('/node')
+  .get((req, res) => {
+    res.send('Course node GET')
+  })
+  .post((req, res) => {
+    res.send('Course node POST')
+  })
+  .put((req, res) => {
+    res.send('Course node PUT')
+  })
+  .delete((req, res) => {
+    res.send('Course node DELETE')
+  })
+
+router.all('/react', (req, res) => {
+  res.send('We are not doing this now')
+})
+
+router.get('/test', (req, res, next) => {
+  console.log('Another');
+  res.send('Success')
+})
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +39,8 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
+
+app.use('/courses', router);
 
 app.use("/users", usersRouter);
 
